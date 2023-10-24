9acef9660c3697700c2852b90a197c868c7d0be7
Otto Coster <48188762+ottocoster@users.noreply.github.com>
2023-10-24 11:02:12 +0200

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,11 +1,30 @@
-const PORT: number = +(process.env.PORT || 8081);
 const NODE_ENV = process.env.NODE_ENV ?? "development";
 
-const server = Bun.serve({
-  port: PORT,
-  fetch() {
-    return new Response("Welcome to Bun!");
+const server = Bun.serve<{ username: string }>({
+  fetch(req, server) {
+    const cookies = req.headers.get("cookie");
+    const username = getUsernameFromCookies(cookies);
+    const success = server.upgrade(req, { data: { username } });
+    if (success) return undefined;
+
+    return new Response("Hello world");
+  },
+  websocket: {
+    open(ws) {
+      const msg = `${ws.data.username} has entered the chat`;
+      ws.subscribe("the-group-chat");
+      ws.publish("the-group-chat", msg);
+    },
+    message(ws, message) {
+      // the server re-broadcasts incoming messages to everyone
+      ws.publish("the-group-chat", `${ws.data.username}: ${message}`);
+    },
+    close(ws) {
+      const msg = `${ws.data.username} has left the chat`;
+      ws.publish("the-group-chat", msg);
+      ws.unsubscribe("the-group-chat");
+    },
   },
 });
 
-console.log(`[${NODE_ENV}] Listening on port ${server.port}`);
+console.log(`[${NODE_ENV}] Listening on ${server.hostname}:${server.port}`);
